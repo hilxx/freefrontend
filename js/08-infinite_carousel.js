6b3e71ad62bd6774e5b5a2f98f1e1e8f9b4ebafb
hilxx <eunjung377@naver.com>
2023-07-10 21:30:30 +0900

    230710_8번

--- js/08-infinite_carousel.js
+++ js/08-infinite_carousel.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
         const button = document.querySelectorAll(".button");
 
         let current = 0; //current를 0으로 초기화하는 것 //가운데
-        let prev = 4; //왼쪽
+        let prev = 2; //왼쪽
         let next = 1; //오른쪽
 
         for(let i = 0; i<button.length; i++) {
@@ -16,9 +16,9 @@ document.addEventListener("DOMContentLoaded", function(){
         }
                                                     //gotoNum(number)
         const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
-        // current가 0보다 클 때(1,2,3,4 => true일 때 gotoNum(current-1) : false일 때 gotoNum(5-1 = 4);)
-        const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
-        // current가 4보다 작을 때(0,1,2,3 => true일 때 gotoNum(current+1) : false일 때 gotoNum(0);)
+        // current가 0보다 클 때(1,2 => true일 때 gotoNum(current-1) : false일 때 gotoNum(2-1 = 1);)
+        const gotoNext = () => current < 2 ? gotoNum(current + 1) : gotoNum(0);
+        // current가 2보다 작을 때(0,1 => true일 때 gotoNum(current+1) : false일 때 gotoNum(0);)
 
         const gotoNum = number => {
             //구버전 function gotoNum(number) {}
@@ -28,19 +28,19 @@ document.addEventListener("DOMContentLoaded", function(){
             next = current + 1; //next에 current+1한 값 대입
 
             for (let i = 0; i < slides.length; i++) {
-                //slides.length = 5
+                //slides.length = 3
                 slides[i].classList.remove("active");
                 slides[i].classList.remove("prev");
                 slides[i].classList.remove("next");
-                //slides가 0번~4번일 때, active,prev,next가 지워지게 해라 = 뒤에 안 보이는 item이 되어라
+                //slides가 0번~2번일 때, active,prev,next가 지워지게 해라 = 뒤에 안 보이는 item이 되어라
             }
 
-            if (next == 5) {
+            if (next == 3) {
                 next = 0;
             }
 
             if (prev == -1) {
-                prev = 4;
+                prev = 2;
             }
 
             slides[current].classList.add("active");
